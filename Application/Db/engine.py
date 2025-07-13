1cfd4d3c84e9671f4a4be14d0a02c29c534b9164
Mani <i.am.ali.zadrahman@gmail.com>
2025-07-14 02:30:16 +0330

    Feature: Add database connection and add new service for MysqlDB in docker-compose file.

---
+++ Application/Db/engine.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "my_sql_db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "check_same_thread": False,
+        "USER": "root",
+        "PASSWORD": "M.1029384756.m",
+        "PORT": 3306
+    },
+)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
